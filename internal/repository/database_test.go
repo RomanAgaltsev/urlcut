a4788ab5ccc4ee4cc10a54facb127d9abf81d375
Roman Agaltsev <roman-agalcev@yandex.ru>
2024-11-05 12:22:55 +0300

    test: add DB repository test

--- internal/repository/database_test.go
+++ internal/repository/database_test.go
@@ -1,1 +1,34 @@
 package repository
+
+import (
+	"database/sql"
+	"github.com/RomanAgaltsev/urlcut/internal/model"
+	"testing"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDBRepository(t *testing.T) {
+	url := &model.URL{
+		Long: "https://app.pachca.com",
+		Base: "http://localhost:8080",
+		ID:   "1q2w3e4r",
+	}
+
+	db, err := sql.Open("pgx", "")
+	require.NoError(t, err)
+
+	dbRepository := NewDBRepository(db)
+
+	err = dbRepository.Store(url)
+	require.NoError(t, err)
+
+	urlG, err := dbRepository.Get("1q2w3e4r")
+	require.NoError(t, err)
+	assert.Equal(t, &model.URL{}, urlG)
+
+	err = dbRepository.Check()
+	assert.True(t, err != nil)
+}
